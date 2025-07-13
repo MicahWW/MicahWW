c168ce57190093495ef1a913606b99577f202a1f
Micah Warren <26724012+MicahWW@users.noreply.github.com>
2025-07-13 14:53:51 -0500

    run on azure

---
+++ server.js
@@ -0,0 +1,17 @@
+import { createServer } from 'http';
+import next from 'next';
+
+const port = parseInt(process.env.PORT || '3000', 10);
+const dev = process.env.NODE_ENV !== 'production';
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+    createServer((req, res) => {
+    handle(req, res);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
