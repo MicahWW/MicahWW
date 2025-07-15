333bdae49aaf4003ef660707411263dbc19eccd7
Micah_Warren <26724012+MicahWW@users.noreply.github.com>
2025-07-14 21:44:47 -0500

    Include hosted location and direct footer links to new tab

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ import styles from "@/styles/footer.module.css";
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-                This page was created with the use of <a href="https://nextjs.org/">Next.js</a> and <a
-                href="https://react.dev/">React</a> with <a href="https://www.typescriptlang.org/">TypeScript</a> and
-                hosted on TBD. To see the source code, visit the <a href="https://github.com/MicahWW/MicahWW">repo on
+                This page was created with the use of <a target="_blank" href="https://nextjs.org/">Next.js</a> and <a target="_blank"
+                href="https://react.dev/">React</a> with <a target="_blank" href="https://www.typescriptlang.org/">TypeScript</a> and
+                hosted on Azure using <a target="_blank" href="https://azure.microsoft.com/en-us/products/app-service">Azure App Service</a>. To see the source code, visit the <a target="_blank" href="https://github.com/MicahWW/MicahWW">repo on
                 GitHub</a>.
         </footer>
     );
