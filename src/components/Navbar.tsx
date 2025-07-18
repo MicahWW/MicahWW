a155066beeaf4c8f17c108fadc42848fd4067032
Micah_Warren <26724012+MicahWW@users.noreply.github.com>
2025-07-18 00:06:37 -0500

    use redirects

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,8 +5,8 @@ const Navbar = () => {
     return (
         <div className={styles.topNav}>
             <Link className={`${styles.topNavLink} ${styles.topNavLinkLeft}`} href="/">Home</Link>
-            <Link className={`${styles.topNavLink} ${styles.topNavLinkLeft}`} target="_blank"  href="https://www.linkedin.com/in/micah-w-warren/">My LinkedIn</Link>
-            <Link className={`${styles.topNavLink} ${styles.topNavLinkLeft}`} target="_blank"  href="https://github.com/MicahWW/">My GitHub</Link>
+            <Link className={`${styles.topNavLink} ${styles.topNavLinkLeft}`} target="_blank"  href="/linkedin">My LinkedIn</Link>
+            <Link className={`${styles.topNavLink} ${styles.topNavLinkLeft}`} target="_blank"  href="/github">My GitHub</Link>
             <Link className={`${styles.topNavLink} ${styles.topNavLinkRight}`} target="_blank" href="https://github.com/MicahWW/MicahWW/">Source Code</Link>
         </div>
     )
