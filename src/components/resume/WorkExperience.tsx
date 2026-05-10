150d727175d2cdf47edd443d264304fa53be19f7
Micah_Warren <26724012+MicahWW@users.noreply.github.com>
2026-05-10 13:55:54 -0500

    collapsible description

--- src/components/resume/WorkExperience.tsx
+++ src/components/resume/WorkExperience.tsx
@@ -1,4 +1,5 @@
 import workExperienceModel from '@/models/resume/workExperienceModel';
+import styles from '@/styles/workExperience.module.css';
 import gStyles from '@/styles/global.module.css';
 
 type workExperienceProps = {
@@ -9,14 +10,14 @@ const WorkExperience = ({ workExperience }: workExperienceProps) => {
     return (
         <div className={gStyles.itemContainer}>
             <span className={gStyles.itemHeader}>{workExperience.jobTitle} - {workExperience.companyName}</span>
-            <span> {workExperience.startDateString()} - {workExperience.endDateString()}</span>
+            <span className={styles.date}>{workExperience.startDateString()} - {workExperience.endDateString()}</span>
             <div>Quick description:</div>
             <ul>
                 {workExperience.descriptionPoints.map((point, index) => (
                     <li key={`point-${index}`}>{point}</li>
                 ))}
             </ul>
-            {(workExperience.description !== null) ?  <><div>Full description:</div><ul><li>{workExperience.description}</li></ul></>  : null }
+            {(workExperience.description !== null) ? <details><summary>Full description:</summary><p className={styles.detailsText}>{workExperience.description}</p></details> : null }
 
         </div>
     );
